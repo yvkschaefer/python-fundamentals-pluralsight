031ef72e148038e321cdc82a4b74274fe0196a94
Kara Schaefer <kara.schaefer@alayacare.com>
2019-01-30 11:23:47 -0500

    Added Docstrings
    access by
    
    python3
    >>> import words
    >>> help(words)

--- words.py
+++ words.py
@@ -1,8 +1,23 @@
+"""Retrieve and print words from a URL.
+
+Usage:
+
+    python3 words.py <URL>
+"""
 import sys
 from urllib.request import urlopen
 
 
 def fetch_words(url):
+    """Fetch a list of words from a URL.
+
+    Args:
+        url: The URL of a UTF-8 text document.
+
+    Returns:
+        A list of strings containing the words from
+        the document.
+    """
     with urlopen(url) as story:
         story_words = []
         for line in story:
@@ -13,13 +28,24 @@ def fetch_words(url):
 
 
 def print_items(items):
+    """Print items one per line.
+
+    Args:
+        An iterable series of printable items.
+    """
     for item in items:
         print(item)
 
 
 def main(url):
+    """Print each word from a text document from a URL.
+
+    Args:
+        url: The URL of a UTF-8 text document.
+    """
     words = fetch_words(url)
     print_items(words)
+
 
 if __name__ == '__main__':
     main(sys.argv[1])
